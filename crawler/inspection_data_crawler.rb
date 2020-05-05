e27d217d7f5b86478e70525c85c49b628cdc9154
TaigaMikami <mikamimikami60@gmail.com>
2020-05-05 23:24:33 +0900

    add crawler inspection_persons.jsonを更新する

---
+++ crawler/inspection_data_crawler.rb
@@ -0,0 +1,34 @@
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+require 'pry'
+
+json_file = "../data/inspection_persons.json"
+
+if __FILE__ == $0
+  url = 'https://www.pref.shimane.lg.jp/bousai_info/bousai/kikikanri/shingata_taisaku/new_coronavirus_portal.html'
+  charset = nil
+  doc = Nokogiri::HTML(open(url), nil, charset)
+
+  insperson = doc.css('table')[0].css('tbody')[0].css('th')[22].text.gsub('人', '')
+  # positive = doc.css('table')[0].css('tbody')[0].css('th')[23].text.gsub('人', '')
+  date = doc.css('table')[0].css('tbody')[0].css('th')[21].text.gsub('日', '')
+  update_time =  DateTime.new(Date.today.year, Date.today.month, date.to_i).iso8601
+
+  json_data = open(json_file) do |f|
+    JSON.load(f)
+  end
+
+  json_date_time = DateTime.parse(json_data['inspection_persons']['labels'].last)
+  update_date_time = DateTime.parse(update_time)
+
+  # jsonファイルのlabelsとスクレイピング日付の比較
+  if (update_date_time.day != json_date_time.day && update_date_time > json_date_time)
+    json_data['inspection_persons']['labels'].push(updateTime)
+    json_data['inspection_persons']['datasets'][0]['data'].push(insperson)
+  end
+
+  open(json_file, 'w') do |f|
+    JSON.dump(json_data, f)
+  end
+end
