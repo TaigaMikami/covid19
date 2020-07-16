9ab70af3683dfd27d4058c045efd4d5434c2a788
TaigaMikami <mikamimikami60@gmail.com>
2020-07-16 23:27:13 +0900

    fix time-bar-chart

--- plugins/vue-chart.ts
+++ plugins/vue-chart.ts
@@ -89,3 +89,66 @@ const createCustomChart = () => {
 }
 
 export default VueChartPlugin
+
+export const scrollPlugin: Chart.PluginServiceRegistrationOptions[] = [
+  {
+    beforeInit(chartInstance) {
+      const fn = () => {
+        if (
+          chartInstance &&
+          chartInstance.canvas &&
+          chartInstance.canvas.parentElement
+        ) {
+          chartInstance.canvas.parentElement.scrollLeft! = chartInstance.width!
+        }
+      }
+      window.addEventListener('resize', fn)
+      fn()
+    }
+  }
+]
+
+const rgba0 = 'rgba(255,255,255,0)'
+const rgba1 = 'rgba(255,255,255,1)'
+export const yAxesBgPlugin: Chart.PluginServiceRegistrationOptions[] = [
+  {
+    beforeDraw(chartInstance) {
+      const ctx = chartInstance.ctx as CanvasRenderingContext2D
+
+      // プロットエリアマスク用
+      ctx.fillStyle = '#fff'
+      ctx.fillRect(
+        0,
+        0,
+        chartInstance.chartArea.left,
+        chartInstance.chartArea.bottom + 1
+      )
+
+      // 横軸マスク用
+      const gradient = ctx.createLinearGradient(
+        0,
+        0,
+        chartInstance.chartArea.left,
+        0
+      )
+      gradient.addColorStop(0, rgba1)
+      gradient.addColorStop(1, rgba0)
+      ctx.fillStyle = gradient
+      ctx.fillRect(
+        0,
+        chartInstance.chartArea.bottom + 1,
+        chartInstance.chartArea.left,
+        (chartInstance.height as number) - chartInstance.chartArea.bottom - 1
+      )
+    }
+  }
+]
+
+export interface DataSets<T = number> extends ChartData {
+  data: T[]
+}
+
+export interface DisplayData<T = number, U = string> {
+  labels?: U[]
+  datasets: DataSets<T>[]
+}
